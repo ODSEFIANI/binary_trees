d0d131569618280e6fe7e6b62583be5fa62d0962
Ayman <65777259+AymanAegon@users.noreply.github.com>
2023-08-29 14:26:16 +0100

    betty fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,34 +7,31 @@
  * @value: the value to put in the new node
  *
  * Return: pointer to the new node, or NULL on failure
- */
- binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
- {
-    
-    binary_tree_t *rchild;
-    binary_tree_t *temp;
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *rchild;
+	binary_tree_t *temp;
 
+	if (parent == NULL)
+		return (NULL);
 
-    if (parent == NULL)
-        return (NULL);
-   
-    rchild = malloc(sizeof(binary_tree_t));
-    if(rchild==NULL)
-        return (NULL);
-    rchild->parent=parent;
-    rchild->n =value;
-    rchild->left=NULL;
-    rchild->right=NULL;
+	rchild = malloc(sizeof(binary_tree_t));
+	if (rchild == NULL)
+		return (NULL);
+	rchild->parent = parent;
+	rchild->n = value;
+	rchild->left = NULL;
+	rchild->right = NULL;
 
-    if(parent->right==NULL)
-        parent->right=rchild;
-    else {  
-        temp=parent->right;
-        parent->right=rchild;
-        rchild->right=temp;
-      
-    }
-    return (rchild);
-
-    }
+	if (parent->right == NULL)
+		parent->right = rchild;
+	else
+	{
+		temp = parent->right;
+		parent->right = rchild;
+		rchild->right = temp;
+	}
+	return (rchild);
+}
  
