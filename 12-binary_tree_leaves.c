2df6d45d6d97a5932b3ebaa14400d4555c4c874d
Ayman <65777259+AymanAegon@users.noreply.github.com>
2023-08-30 11:01:55 +0100

    betty fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,23 +1,26 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - measures the size of a binary tree
- * @tree: a pointer to the root node of the tree to measure the size
- *
- * Return: the size
+* binary_tree_depth - number of level within binary tree
+* @tree: A pointer to the root node of the tree to traverse
+*
+* Return: none
 */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t counter = 0;
-	size_t l, r;
+	size_t counter = 0;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->parent == NULL)
+		return (0);
 
-	l = binary_tree_leaves(tree->left);
-	r = binary_tree_leaves(tree->right);
-	if(tree->left == NULL && tree->right == NULL)
-        counter++;
-    return (counter +l + r);
+	while (tree->parent)
+	{
+		tree = tree->parent;
+		counter++;
+	}
+	return (counter);
 }
