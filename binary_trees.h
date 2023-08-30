15d37694bb52780cb03e547f8052c01ef7a45cf3
AymanAegon <ayo12ammar24@gmail.com>
2023-08-30 06:53:08 -0700

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -19,6 +19,7 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
