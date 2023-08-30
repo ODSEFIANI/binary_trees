a331f55e7f614f96aa4cd893b70bd393651e5aab
AymanAegon <ayo12ammar24@gmail.com>
2023-08-30 03:16:47 -0700

    betty fic

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -17,7 +17,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	l = binary_tree_leaves(tree->left);
 	r = binary_tree_leaves(tree->right);
-	if(tree->left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		counter++;
-	return (counter +l + r);
+	return (counter + l + r);
 }
