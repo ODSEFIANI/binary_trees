3a6a9c3fec17a5e280a0633470cd5fd5de586361
Ayman <65777259+AymanAegon@users.noreply.github.com>
2023-08-30 12:41:03 +0100

    Update 104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -11,8 +11,10 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *left;
 
-	if (!tree || tree->left == NULL)
+	if (!tree)
 		return (NULL);
+	if (tree->left == NULL)
+		return (tree);
 
 	left = tree->left;
 	tree->left = left->right;
