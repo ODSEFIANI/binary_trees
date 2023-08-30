74a42bfc85e7a9dfcddf62b02e2c1680ebf67524
AymanAegon <ayo12ammar24@gmail.com>
2023-08-30 04:08:35 -0700

    task 21

--- binary_trees.h
+++ binary_trees.h
@@ -39,4 +39,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_complete(const binary_tree_t *tree);
+
 #endif
